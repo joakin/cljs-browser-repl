116434ed347a3c288645f0453618edc349da73a3
joakin <joaquin@chimeces.com>
2015-08-06 20:42:20 +0200

    Style changes and autosize repl input
    * Don't compile blank inputs
    * Border box for everything
    * Auto-resize textarea depending on content

--- src/cljs_browser_repl/actions/repl.cljs
+++ src/cljs_browser_repl/actions/repl.cljs
@@ -1,6 +1,7 @@
 (ns cljs-browser-repl.actions.repl
   (:require [cljs-browser-repl.state :as state]
             [cljs-browser-repl.compiler :refer [eval-code empty-compiler-state]]
+            [clojure.string :refer [blank?]]
             ))
 
 (defonce repl-compiler-state (empty-compiler-state))
@@ -8,18 +9,19 @@
 (defn repl-input!
   ([code] (repl-input! code true))
   ([code history?]
-   ; Add just typed command to history
-   (when history?
-     (swap! state/history state/add-entry (state/to-repl-input code)))
-   (eval-code
-     @state/current-ns
-     repl-compiler-state code
-     (fn [{:keys [ns value error] :as ret}]
-       ; Add result to history
-       (when history?
-         (swap! state/history state/add-entry
-                (if error
-                  (state/to-repl-error error)
-                  (do
-                    (reset! state/current-ns ns)
-                    (state/to-repl-result value)))))))))
+   (when-not (blank? code)
+     ; Add just typed command to history
+     (when history?
+       (swap! state/history state/add-entry (state/to-repl-input code)))
+     (eval-code
+       @state/current-ns
+       repl-compiler-state code
+       (fn [{:keys [ns value error] :as ret}]
+         ; Add result to history
+         (when history?
+           (swap! state/history state/add-entry
+                  (if error
+                    (state/to-repl-error error)
+                    (do
+                      (reset! state/current-ns ns)
+                      (state/to-repl-result value))))))))))
--- src/cljs_browser_repl/ui/repl_input.cljs
+++ src/cljs_browser_repl/ui/repl_input.cljs
@@ -1,23 +1,33 @@
-(ns cljs-browser-repl.ui.repl-input)
+(ns cljs-browser-repl.ui.repl-input
+  (:require [clojure.string :as string]))
 
 (defn if-enter
-  "Returns a function that will call f if the event passed in was a 'Enter' key
-  event."
-  [f]
-  (fn [e]
-    (let [shift? (.-shiftKey e)
-          enter? (= (.-key e ) "Enter")]
-      (when enter?
-        (if shift?
-          (set! (.. e -target -value) (str (.. e -target -value) "\n"))
-          (do
-            (f (.. e -target -value))
-            (set! (.. e -target -value) "")))))))
+  "Returns a function that gets an event and will call f if the event passed in
+  was a 'Enter' key event."
+  [f] (fn [e] (when (= (.-key e ) "Enter") (f e))))
+
+(defn enter-pressed!
+  "When shift+enter adds a new line. When only enter it runs the callback
+  function and clears value."
+  [e cb]
+  (let [shift? (.-shiftKey e)
+        v (.. e -target -value)
+        set-val! #(set! (.. e -target -value) %)]
+    (if-not shift?
+      (do (cb (string/trim v)) (set-val! "") (.preventDefault e)))))
+
+(defn change [e]
+  (let [input (.-target e)]
+    (set! (.. input -style -height) "auto")
+    (set! (.. input -style -height) (str (.-scrollHeight input) "px"))))
 
 (defn repl-input [{:keys [pre-label on-input]}]
   [:div.repl-input
    [:span.repl-input-pre pre-label]
    [:textarea.repl-input-input
-    {:on-key-up (if-enter on-input)
+    {:on-key-down (if-enter #(enter-pressed! % on-input))
+     :on-change change
+     :on-input change
      :placeholder "Type clojurescript code here"
-     :rows 1}]])
+     :rows 1
+     }]])
